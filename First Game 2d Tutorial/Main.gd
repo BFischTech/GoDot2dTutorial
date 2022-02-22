314e0fccfa267b7823492d5e7ba12ea7b28e42be
BFischTech <90640795+BFischTech@users.noreply.github.com>
2022-02-22 12:42:59 -0500

    Add files via upload

---
+++ First Game 2d Tutorial/Main.gd
@@ -0,0 +1,73 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	new_game()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func game_over():
+	
+		$ScoreTimer.stop()
+		$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+func _on_MobTimer_timeout():
+	
+	#choose a random location on Path2D
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	
+	#create a mob instance and add it to the scene.
+	var mob = mob_scene.instance()
+	add_child(mob)
+	
+	#set the mob's direction perpendicular to the path direction.
+	#Godot using radians not degrees - PI is about a half turn in radians - 
+	#this is setting it perpendicular by setting it to rotate ~ quarter turn
+
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	#set the mob's position to a random location 
+	#note we're using the var created above
+	
+	mob.position = mob_spawn_location.position
+	
+	#add some randomness to this direction business
+	#note we're using the var created above 
+	#As before this is in radians
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	#choose velocity
+	
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
